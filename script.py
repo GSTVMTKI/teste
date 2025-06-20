8dc77f74e8ec7c2785792ef963eaacc6a410b1b4
Gustavo Miretki <gustavo.miretki@me.com>
2025-06-19 23:01:09 -0300

    update

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 print("hello world")
-
+#dd 
 input_value = input("Enter a value: ")
 print(f"You entered: {input_value}")    
 # This is a simple script that prints "hello world" and prompts the user for input.
